d2f20ad55cc837e0f57b5363d9d552ba0d47cfd5
inyoung <rmdnps10@gmail.com>
2023-08-25 20:20:48 +0900

    페이지 추가 및 라이브러리 다운로드

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import WritePost from "./pages/WritePost";
 import Login from "./pages/Login";
 import Nav from "./components/Nav";
 import GlobalStyles from "./styles";
+import Mypage from "./pages/Mypage";
 function App() {
   return (
     <>
@@ -16,6 +17,8 @@ function App() {
           <Route path="/Write" element={<WritePost></WritePost>}></Route>
           <Route path="/post/:postID" element={<ShowPost></ShowPost>}></Route>
           <Route path="/Login" element={<Login></Login>}></Route>
+          <Route path="/search" element={<Search></Search>}></Route>
+          <Route path="/MyPage" element={<Mypage></Mypage>}></Route>
         </Routes>
       </BrowserRouter>
     </>
