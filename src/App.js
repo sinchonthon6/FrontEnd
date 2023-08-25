bcbc5ba59537c885f9dd5e98db00051e7d171db0
youtheyeon <yuseoyeon@ewhain.net>
2023-08-26 00:00:36 +0900

    style : nav 바 디자인

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Main from "./pages/Main";
-import ShowPost from "./pages/ShowPost";
-import WritePost from "./pages/WritePost";
-import Login from "./pages/Login";
-import Nav from "./components/Nav";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Main from './pages/Main';
+import ShowPost from './pages/ShowPost';
+import WritePost from './pages/WritePost';
+import SearchPost from './pages/SearchPost';
+import Nav from './components/Nav';
+import './App.css';
+
 function App() {
   return (
     <>
       <Nav />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Main></Main>}></Route>
-          <Route path="/Write" element={<WritePost></WritePost>}></Route>
-          <Route path="/post/:postID" element={<ShowPost></ShowPost>}></Route>
-          <Route path="/Login" element={<Login></Login>}></Route>
+          <Route path='/' element={<Main></Main>}></Route>
+          <Route path='/Write' element={<WritePost></WritePost>}></Route>
+          <Route path='/post/:postID' element={<ShowPost></ShowPost>}></Route>
+
+          <Route path='/search' element={<SearchPost></SearchPost>}></Route>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/OAuth.js
@@ -0,0 +1,5 @@
+export const REDIRECT_URI = 'http://localhost:3000/Login/callback/kakao';
+export const KAKAO_TOKEN_URI = 'https://kauth.kakao.com/oauth/token';
+export const KAKAO_USER_URI = 'https://kauth.kakao.com/v2/user/me';
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=2eaa77d8eb767ab77fa0a2c97bd2f81e&redirect_uri=${REDIRECT_URI}&response_type=code
+`;
